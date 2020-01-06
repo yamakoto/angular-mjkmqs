98500c020790ae9148481b47897a1e1a9ce66b34
yamakoto <yamakoto@gmail.com>
2020-01-06 23:32:17 +0900

    3 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
 
+// @Component()デコレーターに注目してください。 
+// これは、次のクラスがコンポーネントであることを示しています。
+// テンプレート、スタイル、セレクターなど、コンポーネントに関するメタデータを提供
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
@@ -7,7 +11,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductAlertsComponent implements OnInit {
 
+  // @Input()デコレーターを使ってproductという名前のプロパティを定義します。
+  // @Inputデコレーターはプロパティ値がコンポーネントの親（この場合は商品リストコンポーネント）から
+  // 渡されることを示します
+  @Input () product;
   constructor() { }
+
 
   ngOnInit() {
   }
